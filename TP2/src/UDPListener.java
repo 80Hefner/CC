149b660fdbfb64aacffb8a58713602e8a066f533
Luís Vieira <luis.vieira36@gmail.com>
2021-05-24 19:11:14 +0100

    testes para core

--- TP2/src/UDPListener.java
+++ TP2/src/UDPListener.java
@@ -22,10 +22,11 @@ public class UDPListener implements Runnable {
                 // Wait for UDP connection from FastFileSrv
                 DatagramPacket data_packet = new DatagramPacket(new byte[4096], 4096);
                 data_socket.receive(data_packet);
-
+                System.out.println("Conexão UDP recebida");
                 // Parse packet data
                 String message = Serializer.Deserialize_String(data_packet.getData());
                 InetAddress address = data_packet.getAddress();
+                System.out.println("InetAddress: " + address);
                 if (message.equals("start connection")) {
                     // Establish UDP connection with FastFileSrv
                     int port = HttpGw.NextPort++;
@@ -36,6 +37,7 @@ public class UDPListener implements Runnable {
                     byte[] send_buf = Serializer.Serialize_Int(port);
                     data_packet = new DatagramPacket(send_buf, send_buf.length, data_packet.getAddress(), data_packet.getPort());
                     data_socket.send(data_packet);
+                    System.out.println("Gateway enviou pacote de resposta");
                 }
                 else if (message.equals("beacon")) {
                     // Process beacon packet
